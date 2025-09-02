294807bbfe0613fab91722bd7bef49287eaa433b
Bhausahebkakad <125125686+Bhausahebkakad@users.noreply.github.com>
2025-09-02 10:31:46 +0530

    Time: 34 ms (20.04%), Space: 45.6 MB (75.58%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        
+        int unique = 1;
+
+        Arrays.sort(candyType);
+
+        for(int i = 0; i < candyType.length-1;i++)
+        {
+            if(candyType[i] != candyType[i+1])
+            {
+                unique++;
+            }
+        }
+        if(unique > candyType.length/2)
+        {
+            return candyType.length/2;
+        }
+
+        return unique;
+    }
+}
