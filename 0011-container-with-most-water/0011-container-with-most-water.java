65ff22497e5c88cb67ca467fd2d8fb62a1e4b188
Bhausahebkakad <125125686+Bhausahebkakad@users.noreply.github.com>
2025-09-26 10:59:28 +0530

    Time: 3 ms (96.19%), Space: 56.9 MB (96.51%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,27 +3,31 @@ class Solution {
         int start = 0;
         int end = height.length-1;
 
-        int arr[] = new int[end+1];
+        // int arr[] = new int[end+1];
         int gap = end;
+
+        int maxVal = 0;
 
         while(start < end)
         {
             if(height[start] < height[end])
             {
-                arr[start] = height[start] * gap;
+                // arr[start] = height[start] * gap;
+                 maxVal = Math.max(height[start] * gap, maxVal);
                 gap--;
                 start++;
             }
             else
             {
-                arr[end] = height[end] * gap;
+               maxVal = Math.max(height[end] * gap, maxVal);
                 gap--;
                 end--;
             }
         }
 
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
 
-        return arr[arr.length-1];
+        // return arr[arr.length-1];
+         return maxVal;
     }
 }
