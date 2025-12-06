40d76e6f3194a61afc955811f9a50211d1008811
Bhausahebkakad <125125686+Bhausahebkakad@users.noreply.github.com>
2025-12-06 18:56:11 +0530

    Time: 24 ms (7.79%), Space: 57.1 MB (5.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,39 +1,45 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        Arrays.sort(nums);
-        int count = 1;
-        int maxCount = 0;
-        int ele = nums[0];
+//         Arrays.sort(nums);
+//         int count = 1;
+//         int maxCount = 0;
+//         int ele = nums[0];
 
-        for(int i = 0; i < nums.length-1; i++)
-{
-            if(nums[i] != nums[i+1])
-            {     
+//         for(int i = 0; i < nums.length-1; i++)
+// {
+//             if(nums[i] != nums[i+1])
+//             {     
                 
-                if(maxCount < count)
-                {
-                    maxCount = count;
-                    ele = nums[i];
-                }
+//                 if(maxCount < count)
+//                 {
+//                     maxCount = count;
+//                     ele = nums[i];
+//                 }
 
-                count = 1;
-            }
-            else
-            {
-                count++;
-            }
+//                 count = 1;
+//             }
+//             else
+//             {
+//                 count++;
+//             }
 
-        }
-         if(maxCount < count)
-                {
-                    maxCount = count;
-                    ele = nums[nums.length-1];
-                }
+//         }
+//          if(maxCount < count)
+//                 {
+//                     maxCount = count;
+//                     ele = nums[nums.length-1];
+//                 }
 
-        return ele;
+//         return ele;
 
-
+        return Arrays.stream(nums).boxed()
+                .collect(Collectors.groupingBy(num-> num, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get()
+                .getKey();
       
     }
 }
