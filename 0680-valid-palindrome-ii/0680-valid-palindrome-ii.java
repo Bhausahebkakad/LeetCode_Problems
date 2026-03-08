67b00f9b28398d3d0b22480ec5c1db7f2dc5b3df
Bhausahebkakad <125125686+Bhausahebkakad@users.noreply.github.com>
2026-03-08 13:38:12 +0530

    Time: 4 ms (99.59%), Space: 47.5 MB (96.4%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        
+        int left = 0;
+        int right = s.length()-1;
+      
+        while(left <= right)
+        {
+           if(s.charAt(left) == s.charAt(right))
+           {
+             left++;
+             right--;
+           }
+             else 
+            {
+                 return checkPalindrome( s,  left+1,  right)
+                || checkPalindrome( s,  left,  right-1);
+            }
+        }
+        return true;
+    }
+    public static boolean checkPalindrome (String s, int left, int right)
+    {
+       while(left <= right)
+       {
+         if(s.charAt(left) == s.charAt(right))
+         {
+            left++;
+            right--;
+         }
+         else return false;
+         
+       }
+        return true;
+    }
+}
