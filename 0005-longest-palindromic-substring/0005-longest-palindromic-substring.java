dd33e42026a5dde0ad337624439f1744295d7c07
Bhausahebkakad <125125686+Bhausahebkakad@users.noreply.github.com>
2026-04-02 22:47:35 +0530

    Time: 26 ms (30.75%), Space: 43.5 MB (85.1%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,75 @@
 class Solution {
     public static String longestPalindrome(String s) {
 
-        if (s == null || s.length() < 1) return "";
+    //     if (s == null || s.length() < 1) return "";
 
-        int start = 0, end = 0;
+    //     int start = 0, end = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+    //     for (int i = 0; i < s.length(); i++) {
 
-            int len1 = expand(s, i, i);      
-            int len2 = expand(s, i, i + 1);  
+    //         int len1 = expand(s, i, i);      
+    //         int len2 = expand(s, i, i + 1);  
 
-            int len = Math.max(len1, len2);
+    //         int len = Math.max(len1, len2);
 
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
-        }
+    //         if (len > end - start) {
+    //             start = i - (len - 1) / 2;
+    //             end = i + len / 2;
+    //         }
+    //     }
 
-        return s.substring(start, end + 1);
-    }
+    //     return s.substring(start, end + 1);
+    // }
 
-    public static int expand(String s, int left, int right) {
+    // public static int expand(String s, int left, int right) {
 
-        while (left >= 0 && right < s.length()
-                && s.charAt(left) == s.charAt(right)) {
+    //     while (left >= 0 && right < s.length()
+    //             && s.charAt(left) == s.charAt(right)) {
 
-            left--;
-            right++;
-        }
+    //         left--;
+    //         right++;
+    //     }
 
-        return right - left - 1;
+    //     return right - left - 1;
+
+        // String str = s;
+
+        int start = 0;
+         int end = 0;
+         
+         for(int i = 0; i < s.length(); i++)
+         {
+             char ch = s.charAt(i);
+             
+             int left = i;
+             int right = i;
+             
+             while(left >=0 && s.charAt(left) == ch)
+             {
+                 left--;
+             }
+             while(right < s.length() && s.charAt(right) == ch)
+             {
+                 right++;
+             }
+             while(left >=0 && right < s.length())
+             {
+                 if(s.charAt(left) != s.charAt(right))
+                 {
+                     break;
+                 }
+                 left--;
+                 right++;
+             }
+             left++;
+             while(end - start < right - left)
+             {
+                 start = left;
+                 end = right;
+             }
+             
+         }
+         return s.substring(start , end);
     }
 
 }
